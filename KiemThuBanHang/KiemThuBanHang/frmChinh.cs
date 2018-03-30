28a4d9a9d560bc7a046eebd06e14ca52e91b458c
ngngochanh96 <35825315+ngngochanh96@users.noreply.github.com>
2018-03-30 12:19:44 +0700

    them form Chinh, form Thuc Don, form order

--- KiemThuBanHang/KiemThuBanHang/frmChinh.cs
+++ KiemThuBanHang/KiemThuBanHang/frmChinh.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace KiemThuBanHang
 {
-    public partial class Form1 : Form
+    public partial class frmChinh : Form
     {
-        public Form1()
+        public frmChinh()
         {
             InitializeComponent();
+        }
+
+        private void frmChinh_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
